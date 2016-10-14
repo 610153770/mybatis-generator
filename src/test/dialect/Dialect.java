0a15f544a063a74912fb97d514873863f7c500a8
chenyong <chenyong@530e6178-1d98-482f-84e4-ecf96674a383>
2016-10-14 03:55:48 +0000

    o2o基础服务

---
+++ src/test/dialect/Dialect.java
@@ -0,0 +1,11 @@
+package dialect;
+
+public abstract class Dialect {
+
+	public static enum Type {
+		MYSQL, ORACLE, SQLSERVER
+	}
+
+	public abstract String getLimitString(String querySqlString, int offset, int limit);
+
+}
